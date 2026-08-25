030748cf6c09e32b6e869b3fee4c77ab30a2f583
Jasmin Bautista Esperida <esperidajasmin2@gmail.com>
2026-08-25 23:55:30 +0800

    Deploy site to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.GITHUB_ACTIONS ? "/pags-website/" : "/",
   build: {
     outDir: "dist",
     sourcemap: false,
